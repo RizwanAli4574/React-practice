90069cdfa99708a8fe10b2d53626adda933a4156
RizwanAli4574 <rizwanali4574992@gmail.com>
2025-10-16 17:21:31 +0500

    Practice 16-10-2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import Bye from "./Bye"
-import Hello from "./Hello"
 
+import Hello from "./Hello"
+import Button from './Button'
 
 function App(){
+
+  const hobbies = ["Reading" , "Coding",  "Traveling"]
+
+  function Message(){
+    alert("Hello from message")
+  }
+
+  function ByeMessage(){
+    alert("Bye from message")
+  }
+
 return (
   <>
-  <Hello />
-  <Bye />
+  <Hello name="Rizwan Ali" age={25} city="Lahore" hobbies={hobbies}/>
+  <Button label="Click Me" handleClick={Message}/>
+  <br/>
+  <br/>
+  <Button label="Just Click" handleClick={ByeMessage}/>
+  
   </>
 )
 }
--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,59 +1,18 @@
+function Hello({ name="Guest", age=34 , city="Hanif Park" , hobbies} ) {
+  
 
-
-
-
-// const myName = (yourname) => {
-// return yourname
-// }
-
-// const handleClick = () => {
-//     alert("Click Ho gya")
-// }
-// const handleInput = (event) =>{
-// console.clear()
-// console.log("value :" , event.target.value)
-// }
-
-// const handleMouseOver = () => console.log("Mouse Over")
-
-// const handleDoubleClick = () => console.log("Double Click")
-
-
-// const name = "Rizwan Ali"
-// const name1 = "waqar Ali"
-
-// return (
-
-//     <>
-//     <h1>Hello {myName(name)}</h1>
-//     <h2>Hello {myName(name1)}</h2>
-
-//     <p onMouseOver={handleMouseOver} onDoubleClick={handleDoubleClick}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-
-//     <button onClick={handleClick}>Click Me</button>
-//     <button onClick={()=>{alert("Hello")}}>Say Hello</button>
-//     <br />
-//     <input type="text"  onChange={handleInput} placeholder="Type something"/>
-//     </>
-// )
-
-function Hello(){
-
-const fruits = ["Apple" , "Banana", "Orange"]
-
-return (
-
+  return (
     <>
-    <h2>Fruit List</h2>
-    <ul>
-        {fruits.map((fruit, index) => (
-            <li>{index} - {fruit}</li>
-
+      <h1>Hello {name}</h1>
+      <h2>Age:  {age}</h2>
+      <h3>City: {city}</h3>
+      <ul>
+        {hobbies.map((hobby, index)=>(  
+          <li key={index}>{hobby}</li>
         ))}
-    </ul>
+      </ul>
     </>
-)
-
+  );
 }
 
-export default Hello
+export default Hello;
